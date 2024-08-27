b577a67a58bed0e085a8e7205b0fd292fbbd4be6
geordym <ggeordymm@gmail.com>
2024-08-27 17:41:24 -0500

    Starting

---
+++ src/main/java/com/emazon/emazon_usuario/EmazonUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.emazon_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/emazon_usuario/EmazonUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.emazon_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
